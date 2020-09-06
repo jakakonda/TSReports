dd42290c31380d1996e9de2ada0fe9bd9a2eb6e9
Jaka Konda <jaka@konda.si>
2020-09-06 16:22:03 +0200

    Code cleanup

--- app/exporters/HtmlExporter.ts
+++ app/exporters/HtmlExporter.ts
@@ -8,14 +8,12 @@ export class HtmlExporter implements IPdfExporter {
         return new Promise<Stream>((res, err) => {
             const doc = new JSDOM(html);
             const dir = path.basename(fullPath);
-            console.log("AAA");
             console.log(dir);
             const document = doc.window.document;
 
             const head = document.querySelector('head');
             const base = document.createElement('base');
             base.setAttribute('href', `/templates/${dir}/`);
-            // head.appendChild(base);
             head.insertBefore(base, head.firstChild);
 
             const readable = Readable.from([doc.serialize()]);
